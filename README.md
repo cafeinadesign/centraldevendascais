1ec43cfc9af8ad9a19f01ec6395a92680ba65f77
Thiago Prazeres <thiagoprazeres@gmail.com>
2021-04-28 20:37:24 -0300

    start

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Centraldevendascais
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.8.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.10.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,9 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    ScullyLibModule
+    AppRoutingModule,
+    ScullyLibModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
