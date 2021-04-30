cd533535d50b440e8687706b3b5c3405e724a638
Thiago Prazeres <thiagoprazeres@gmail.com>
2021-04-30 12:04:40 -0300

    fotos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';// You can specify which plugins you need
+import 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  ngOnInit(){}
-}
+export class AppComponent {}
